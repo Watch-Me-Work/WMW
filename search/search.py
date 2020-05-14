2af65b682885e97be3009a2ac907d24bdf10ffbc
ntantwan <ntantwan@gmail.com>
2020-05-14 14:08:45 -0400

    Integrating vectorizing search into main search

--- search/search.py
+++ search/search.py
@@ -1,6 +1,7 @@
 import os
 import requests
 
+
 class FoundDocument:
     def __init__(self, title):
         self.title = title
@@ -28,10 +29,40 @@ class BingSearchEngine:
         except Exception as ex:
             raise ex
 
-class DummySearchEngine:
-    def __init__(self):
-        pass
 
-    def search_by_string(self, query_string):
-        titles_const = ['Southern Hemisphere - Wikipedia', 'The Differences Between Northern & Southern Hemisphere ...', 'Geography of the Southern Hemisphere and Facts', 'Which Countries Are in the Southern Hemisphere ...', 'Countries Located Completely in the Southern Hemisphere ...', "A Beginner's Guide to the Southern Hemisphere Sky - Sky ...", 'Are we prepared for COVID-19 hitting the southern hemisphere?', 'Southern Hemisphere | Definition of Southern Hemisphere by ...', 'Southern hemisphere | Definition of Southern hemisphere at ...', 'Southern Constellations – Constellation Guide'] 
-        return [FoundDocument(title) for title in titles_const]
+
+class CorpusSearchEngine:
+    def __init__(self, vectorize_fn):
+        self._vectorize_fn = vectorize_fn
+        self._corpus = self.corpus_loader()
+        self._corpuslist = ['psyonix', 'codwarzone', 'lastofus', 'larianstudios']
+        if self._vectorize_fn == "lsi":
+            self.corpus_bow()
+       # if vec_func == tfidf
+       # corpus = ...
+
+    def corpus_loader(self):
+        from gensim.utils import simple_preprocess
+        docs = [open('wiki/psyonix.txt', 'r').read(), open('wiki/cod-warzone.txt', 'r').read(), open('wiki/last-of-us-2.txt', 'r').read(), open('wiki/larian-studios.txt', 'r').read()]
+        for doc in docs:
+            doc.replace("\n", " ")
+        return docs
+
+    def corpus_bow(self):
+        from gensim.utils import simple_preprocess
+        from gensim import corpora
+        tokenizer = [simple_preprocess(doc) for doc in corpus]
+        corpusbow = corpora.Dictionary()
+        self._corpus = [corpusbow.doc2bow(doc, allow_update=True) for doc in tokenizer]
+
+    def search_by_string(self, string):
+        if self._vectorize_fn == "lsi":
+            from gensim import models
+            from gensim import similarities
+            lsi = models.LsiModel(self._corpus)
+            index = similarities.MatrixSimilarity(lsi[self._corpus])
+            vec_bow = dict.doc2bow(string.lower().split())
+            vec_lsi = lsi[vec_bow]
+            sims = index[vec_lsi]
+            sims = sorted(enumerate(sims), key=lambda item: -item[1])
+            return self._corpuslist[sims[0][0]][0:10]
