24b6508db2299172191f8b5f375723545d67a961
Mike D'Arcy <m.m.darcy@u.northwestern.edu>
2020-05-16 11:40:15 -0500

    Make server more configurable

--- run_server.py
+++ run_server.py
@@ -1,3 +1,4 @@
+import argparse
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 import time
@@ -17,12 +18,10 @@ class WmwRequestHandler(BaseHTTPRequestHandler):
             self.wfile.write(json.dumps({'error': 'unknown endpoint'}).encode('utf-8'))
 
         request_params = json.loads(self.rfile.read(int(self.headers['content-length'])).decode('utf-8'))
-        finder = BingRelatedDocumentFinder()
-        # FIXME: This BS4Extractor returns Response with empty text for tests/test_request_wiki
-        #extractor = BS4Extractor()
-        extractor = H2TExtractor()
-        tmp = extractor.extractFromHTML(request_params['document_html'])
-        result = finder.search(tmp)
+        finder = self.server.doc_finder
+        extractor = self.server.text_extractor
+        doc_content = extractor.extractFromHTML(request_params['document_html'])
+        result = finder.search(doc_content)
         result = {'results': [{'title': r.get_title()} for r in result]}
 
         self.send_response(200)
@@ -30,9 +29,34 @@ class WmwRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(json.dumps(result).encode('utf-8'))
 
+class WmwServer(HTTPServer):
+    def __init__(self, cmdargs, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cmdargs = cmdargs
+
+        if self.cmdargs.parser_type == 'h2t':
+            self.text_extractor = H2TExtractor()
+        elif self.cmdargs.parser_type == 'bs4':
+            self.text_extractor = BS4Extractor()
+        else:
+            raise ValueError("Unknown parser type")
+
+        if self.cmdargs.finder_type == 'bing':
+            self.doc_finder = BingRelatedDocumentFinder()
+        elif self.cmdargs.finder_type == 'corpus':
+            self.doc_finder = CorpusRelatedDocumentFinder()
+        else:
+            raise ValueError("Unknown finder type")
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--parser_type', choices=['h2t', 'bs4'], default='bs4')
+    parser.add_argument('--finder_type', choices=['bing', 'corpus'], default='corpus')
+    args = parser.parse_args()
+
     print(f"Starting server on host={HOSTNAME}, port={WMW_PORT}")
-    server = HTTPServer((HOSTNAME, WMW_PORT), WmwRequestHandler)
+    server = WmwServer(cmdargs=args, server_address=(HOSTNAME, WMW_PORT), RequestHandlerClass=WmwRequestHandler)
 
     server.serve_forever()
 
