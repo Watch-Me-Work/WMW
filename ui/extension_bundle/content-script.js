1d07162f8390ed5e0932a835add22c65bf15839f
Yousef Issa <yousef@u.northwestern.edu>
2020-05-19 16:18:11 -0500

    moving from popup to sidebar flow

---
+++ ui/extension_bundle/content-script.js
@@ -0,0 +1,29 @@
+chrome.runtime.onMessage.addListener(function(msg, sender){
+    if(msg == "toggle"){
+    	    	console.log("HELLO THIS WAS TOGGLED")
+        toggle();
+    }
+})
+
+var iframe = document.createElement('iframe'); 
+iframe.style.background = "green";
+iframe.style.height = "100%";
+iframe.style.width = "0px";
+iframe.style.position = "fixed";
+iframe.style.top = "0px";
+iframe.style.right = "0px";
+iframe.style.zIndex = "9000000000000000000";
+iframe.frameBorder = "none"; 
+iframe.src = chrome.extension.getURL("sidebar.html")
+
+document.body.appendChild(iframe);
+
+function toggle(){
+    if(iframe.style.width == "0px"){
+    	console.log(" THIS WAS TOGGLED INSIDE")
+        iframe.style.width="400px";
+    }
+    else{
+        iframe.style.width="0px";
+    }
+}
