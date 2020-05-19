b4f8dcc7c1cd9428b60ab9b31356fd88846de117
Mike D'Arcy <m.m.darcy@u.northwestern.edu>
2020-05-19 14:37:20 -0500

    Fix permission bug in sending backend request

--- ui/extension_bundle/bundle.js
+++ ui/extension_bundle/bundle.js
@@ -34,7 +34,7 @@ const fetch = require("node-fetch");
 var textToSend = {"document_html": "Penguins (order Sphenisciformes, family Spheniscidae) are a group of aquatic flightless birds. They live almost exclusively in the Southern Hemisphere, with only one species, the Galápagos penguin, found north of the equator. Highly adapted for life in the water, penguins have countershaded dark and white plumage and flippers for swimming. Most penguins feed on krill, fish, squid and other forms of sea life which they catch while swimming underwater. They spend roughly half of their lives on land and the other half in the sea."};
 
 // summarize and send back
-const api_url = 'http://localhost:3380';
+const api_url = 'http://localhost:3380/find_related';
 
 
 function summarize() {
@@ -47,7 +47,8 @@ function summarize() {
 	  .then(data => { 
 		  return data.json() })
 	  .then(res => { 
-		  console.log(res)
+          alert(JSON.stringify(res));
+		  console.log(res);
 	   })
 	  .catch(error => console.error('Error:', error));
 }
