c595af28053bc029231f844a486f086c5888ebfc
AlinHulli <49260013+AlinHulli@users.noreply.github.com>
2020-05-26 14:51:24 +0300

    edited for getting page content

--- ui/extension_bundle/bundle.js
+++ ui/extension_bundle/bundle.js
@@ -27,15 +27,14 @@ exports.Response = global.Response;
 },{}],2:[function(require,module,exports){
 
 const fetch = require("node-fetch");
-
-// capture all text
-//var textToSend = document.body.innerText;
-
-var textToSend = {"document_html": "Penguins (order Sphenisciformes, family Spheniscidae) are a group of aquatic flightless birds. They live almost exclusively in the Southern Hemisphere, with only one species, the Galápagos penguin, found north of the equator. Highly adapted for life in the water, penguins have countershaded dark and white plumage and flippers for swimming. Most penguins feed on krill, fish, squid and other forms of sea life which they catch while swimming underwater. They spend roughly half of their lives on land and the other half in the sea."};
-
-// summarize and send back
 const api_url = 'http://localhost:3380/find_related';
+var textToSend = null
 
+window.addEventListener("message", function(e){
+	textToSend = {"document_html": e.data};
+	summarize()
+    
+}, false);
 
 function summarize() {
 	fetch(api_url, {
@@ -47,8 +46,7 @@ function summarize() {
 	  .then(data => { 
 		  return data.json() })
 	  .then(res => { 
-          alert(JSON.stringify(res));
-		  console.log(res);
+		  console.log(res)
 	   })
 	  .catch(error => console.error('Error:', error));
 }
